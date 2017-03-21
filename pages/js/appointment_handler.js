5c793aad7e2585e337e4bd9a8628c53e292a84f5
Mitchell S <Mitchell S>
2017-03-21 00:14:22 -0600

    Deleting old files before I upload the new ones

--- pages/js/appointment_handler.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", stylistListeners);
-//document.getElementById("newCustomerAppointment").addEventListener("click", addCustomerAppointment);
-document.getElementById("selectHairstyle").addEventListener("blur", verifyHairstyle);
-document.getElementById("selectLocation").addEventListener("blur", verifyLocation);
-document.getElementById("selectedHairstylist").addEventListener("click", openHairstylistWindow);
-document.getElementById("specialRequest").addEventListener("keyup", verifySpecialRequest);
-document.getElementsByClassName("close")[0].addEventListener("click",closeSelectHairstylist);
-document.getElementsByClassName("close")[0].addEventListener("click",verifyStylist);
-document.getElementsByClassName("close")[1].addEventListener("click",closeEvent);
